dc5fd89a481beb2c7c618506bde3dbe2c79494e3
Atharv140903 <atharvkurde2003@gmail.com>
2023-03-02 15:33:09 +0530

    bootstrap added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -20,7 +22,9 @@ import { JobCreateComponent } from './job-create/job-create.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
